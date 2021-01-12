f9aa6ada72cfd24f219e1db9332063d9f8d2157f
darshan2429 <darshannb.xworkz@gmail.com>
2021-01-12 21:06:32 +0530

    adding array program

---
+++ Malls.java
@@ -0,0 +1,14 @@
+class Malls
+{
+	public static int noOfShopes=20;
+	public static String shopNames[]={"AllenSolly","Max","Forver 21","PeterEngland"};
+	public static void main(String a[])
+	{
+		System.out.println(noOfShopes);
+		System.out.println(shopNames[0]);
+		System.out.println(shopNames[1]);
+		System.out.println(shopNames[2]);
+		System.out.println(shopNames[3]);
+		System.out.println(shopNames.length);
+	}
+}
