15761848f49144e65781548b93b727de22edccbf
darshan2429 <darshannb.xworkz@gmail.com>
2021-01-19 14:58:36 +0530

    array program

--- JokerTester.java
+++
@@ -1,8 +0,0 @@
-class JokerTester
-{
-	public static void main(String a[])
-	{
-		JokerDTO joker=new JokerDTO("darshan","shivamogga",8845465200L);
-		System.out.println(joker.getName()+" "+joker.getAddress()+" "+joker.getMobileNo());
-	}
-}
